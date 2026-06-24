def4dbe9469ea13cecad62cadfc203ee0d9c47fc
sammy <samuel@kajetsky.com>
2026-06-24 17:48:42 +0300

    Update wofi-cliphist.sh

--- .config/wofi/wofi-cliphist.sh
+++ .config/wofi/wofi-cliphist.sh
@@ -1,4 +1,8 @@
 CHOICE=$(cliphist list | wofi --dmenu --prompt "Clipboard history")
 [ -z "$CHOICE" ] && exit 0
-printf "%s" "$CHOICE" | wl-copy
-printf "%s" "$CHOICE" | wl-copy --primary
+
+CONTENT=${CHOICE#*[0-9]}
+CONTENT=$(printf '%s\n' "$CHOICE" | sed -E 's/^[[:space:]]*[0-9]+[[:space:]]+//')
+
+printf '%s' "$CONTENT" | wl-copy
+printf '%s' "$CONTENT" | wl-copy --primary
