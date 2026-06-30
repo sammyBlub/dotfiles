7b4ceb80553a4c7872530c7a83a9fd9b4801d01e
sammy <samuel@kajetsky.com>
2026-06-30 23:11:55 +0300

    Delete .config/wofi/wofi-cliphist.sh

--- .config/wofi/wofi-cliphist.sh
+++
@@ -1,8 +0,0 @@
-CHOICE=$(cliphist list | wofi --dmenu --prompt "Clipboard history")
-[ -z "$CHOICE" ] && exit 0
-
-CONTENT=${CHOICE#*[0-9]}
-CONTENT=$(printf '%s\n' "$CHOICE" | sed -E 's/^[[:space:]]*[0-9]+[[:space:]]+//')
-
-printf '%s' "$CONTENT" | wl-copy
-printf '%s' "$CONTENT" | wl-copy --primary
